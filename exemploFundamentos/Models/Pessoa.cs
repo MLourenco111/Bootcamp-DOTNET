0904b466980df6b694cfd219ed357a0f667bba35
MLourenco111 <matheuslourenco111@gmail.com>
2023-09-04 11:09:12 -0300

    Pasta exemplo
    Criacao da pasta exemplo, models e da classe Pessoa

---
+++ exemploFundamentos/Models/Pessoa.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+//uma organizacao das suas classes, representa um caminho logico que esteja no mesmo dominio
+namespace exemploFundamentos.Models{
+    public class Pessoa{
+        // atributos da classe pessoa 
+        public string? Nome { get; set; } //get serve para pegar um valor, set para atribuir valor
+        public int Idade { get; set; }
+        public string? NomeRepresentanteLegal { get; set; } 
+        // metodos da classe pessoa
+        public void Apresentar(){
+        // comando para escrever no terminal
+        Console.WriteLine($"Olá, meu nomé é {Nome}, e tenho {Idade} anos");
+        }
+    }
+}
+
+
+
+// @ permite a atribuicao de palavras reservadas da maneira que quiser
+//É possivel ter mais de uma classe com o mesmo nome, mas o namespace deve ser diferente
+// PascalCase é usado em classes, metodos e atributos e camelCase é usado nas variaveis
---
+++ exemploFundamentos/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Passando o caminho da classe (namespace)
+using exemploFundamentos.Models;
+
+//criando uma instancia da classe Pessoa
+Pessoa p1 = new Pessoa();
+//adicionando valores para os atributos
+p1.Nome = "Matheus";
+p1.Idade = 23;
+//chamada de metodo
+p1.Apresentar();
