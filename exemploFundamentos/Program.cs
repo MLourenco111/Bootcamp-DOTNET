cc9115f2fef4bf66af9a3e99117855821af6e8e1
MLourenco111 <matheuslourenco111@gmail.com>
2023-09-06 17:07:08 -0300

    operadores
    operadores e como utilizar

--- exemploFundamentos/Program.cs
+++ exemploFundamentos/Program.cs
@@ -9,3 +9,51 @@ p1.Nome = "Matheus";
 p1.Idade = 23;
 //chamada de metodo
 p1.Apresentar();
+
+Console.WriteLine("-----------------------");
+// tipos de dados
+string frase = "Olá eu sou uma frase";
+int quantidade = 10;
+double altura = 1.70;
+decimal preco = 1.80M;
+bool condicao = true;
+
+
+Console.WriteLine(frase);
+Console.WriteLine("valor da quantidade " + quantidade);
+Console.WriteLine("valor da altura " + altura.ToString("0.00"));
+Console.WriteLine("valor da preco " + preco);
+Console.WriteLine("valor da condicao " + condicao);
+
+// tipo date time
+
+  DateTime dataAtual = DateTime.Now.AddDays(5).AddHours(10);
+  Console.WriteLine(dataAtual);
+  Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+
+
+//operadores
+  // = atribui um valor para a variavel
+
+
+ int a =10 , b= 10 , c=0;
+ c = a + b;
+ 
+ c = c + 5; // 20 + 5
+ // os dois fazer a mesma coisa de forma diferente
+ c += 5;// 25 + 5
+ c -= 5; // 30 - 5
+
+  Console.WriteLine(c);
+
+  //converter os tipos de variaveis
+  //a converção é chamada de casting      
+
+  int valor = Convert.ToInt32("5");
+//parse é universal para qualquer tipo de dados
+  int valor1 = int.Parse("5");
+
+// a diferenca entre convert e parse é o tratamento com o valor null, o convert converte para 0 e parse nao aceita nulo dando erro
+
+  Console.WriteLine(valor);
+  Console.WriteLine(valor1);
